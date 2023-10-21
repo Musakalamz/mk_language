589d4ef913c39a13cc4cca4f3f7d3e51dc18cb30
Musakalamz <ogunsolumusa@gmail.com>
2023-10-21 18:31:25 +0100

    Programmer fist step to java

---
+++ 00-hello_world/print.java
@@ -0,0 +1,12 @@
+/*
+ * File: print.java
+ * Auth: Musa_kalamz A Ogunsolu
+ */
+
+public class print
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\"Programming is like building a multilingual puzzle");
+	}
+}
---
+++ 00-hello_world/puts.java
@@ -0,0 +1,17 @@
+/*
+ * File: puts.java
+ * Auth: Musa_kalamz A Ogunsolu
+ */
+
+public class puts
+{
+
+/*
+ * Main - Prints with proper grammar, but the outcome is a piece of art
+ */
+
+	public static void main(String[] args)
+	{
+		System.out.println("with proper grammar, but the outcome is a piece of art");
+	}
+}
